0ea630e8aed3155eacef79e6eba12c132d0bbca3
Andrew Huggins <ahuggins@torchtechnologies.com>
2023-01-18 21:34:58 -0600

    formatted to use beamer

--- src/clj_presentation/core.clj
+++ src/clj_presentation/core.clj
@@ -18,4 +18,30 @@
 ;; unofficial hosts
 ;; unique libraries
 
-[1 2 3 4]
+(defn my-sum
+  ([v] (my-sum v 0))
+
+  ([v acc]
+   (if (empty? v)
+     acc
+     (my-sum (rest v) (+ acc (first v))))))
+
+(my-sum [1 2 3 4])
+;; => 10
+(my-sum (range 9999)) ;;overflow
+
+(defn my-sum-recur
+  ([v] (my-sum-recur v 0))
+
+  ([v acc]
+   (if (empty? v)
+     acc
+     (recur (rest v) (+ acc (first v)))))
+  )
+
+(my-sum-recur (range 9999))
+
+(reduce + 0 [1 2 3 4])
+;; => 10
+(reductions + 0 [1 2 3 4])
+;; => (0 1 3 6 10)
